ad415f6ea2bb2694e0babd9871e1e1a2e328d045
hiratsukashu <hiratsukashu@indival.co.jp>
2015-05-01 10:17:45 +0900

    first commit

--- WebHockSample/ViewController.swift
+++ WebHockSample/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIWebViewDelegate {
 
+    @IBOutlet weak var webview: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let URL = NSURL(string: "http://calm-river-1648.herokuapp.com/")
+        var urlRequest: NSURLRequest = NSURLRequest(URL: URL!)
+        //self.webview.delegate = self
+        self.webview.loadRequest(urlRequest)
     }
 
+    
+    func webViewDidStartLoad(webView: UIWebView){
+        println("start")
+        
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView){
+        println("finish")
+    }
+    
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError){
+        
+        
+    }
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool{
+        
+        let urlstr = request.URL?.absoluteString!
+        let urlscheme = request.URL?.scheme!
+        //println(urlstr)
+        println(urlscheme)
+        if urlscheme == "hiratsuka" {
+            println("success")
+            
+            
+            
+            return false
+        }
+        
+        return true
+        
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
